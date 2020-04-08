1251153778ee5d15234b169d961c4dbe7acf46a6
angelo.lugue@eee.upd.edu.ph <m@sterl3arner>
2020-04-08 09:19:44 +0800

    add dice images, comment on index.js entrypoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //entry point of the React site
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
