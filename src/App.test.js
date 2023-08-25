8448ac4a75589efc226f4c7ef4906b71fd8e59c8
lalugue <51893731+lalugue@users.noreply.github.com>
2023-08-25 16:50:31 +0800

    @TODO: add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+// @TODO: add more tests
+
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const buttonElement = getByText(/Roll Dice/i);
